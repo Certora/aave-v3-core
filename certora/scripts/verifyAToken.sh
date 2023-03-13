56b7682e52789a9858d3eabb4a187e9bac16738f
Michael M <MichaelM@certora.com>
2023-03-13 09:58:33 +0200

    fixing job fail in CI due to flag deprecation

--- certora/scripts/verifyAToken.sh
+++ certora/scripts/verifyAToken.sh
@@ -2,8 +2,8 @@ certoraRun certora/harness/SimpleERC20.sol \
             certora/harness/ATokenHarness.sol \
     --verify ATokenHarness:certora/specs/AToken.spec \
     --link ATokenHarness:_underlyingAsset=SimpleERC20 \
-    --settings -enableGhostGrounding=true \
     --optimistic_loop \
     --solc solc8.10 \
     --cloud master \
+    --rule_sanity \
     --msg "aToken spec - all rules" 
