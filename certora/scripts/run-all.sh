ee590ed6d93cfd4808c6716d7e39d9b9657cd620
nisnislevi <nissan@certora.com>
2024-03-26 18:22:19 +0200

    add some of Tadeas rules. (the rules who managed to pass.)

--- certora/scripts/run-all.sh
+++ certora/scripts/run-all.sh
@@ -1,4 +1,4 @@
-CMN=""
+CMN="--typecheck_only"
 
 
 
@@ -25,4 +25,55 @@ certoraRun $CMN certora/conf/UserConfigCLI.conf \
 echo "******** Running:  6 ***************"
 certoraRun $CMN certora/conf/VariableTokenCLI.conf \
            --msg "6: VariableTokenCLI.conf"
+
+echo "******** Running:  7 NEW no summarization ***************"
+certoraRun $CMN certora/conf/NEW-pool-no-summarizations.conf
+           --msg "7: NEW-pool-no-summarizations"
+
+
+echo "******** Running:  simple:1 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotDepositInInactiveReserve \
+           --msg "simple:1: NEW :: cannotDepositInInactiveReserve"
+
+echo "******** Running:  simple:2 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotDepositInFrozenReserve \
+           --msg "simple:2: NEW :: cannotDepositInFrozenReserve"
+
+echo "******** Running:  simple:3 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotDepositZeroAmount \
+           --msg "simple:3: NEW :: cannotDepositZeroAmount"
+
+echo "******** Running:  simple:4 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotWithdrawZeroAmount \
+           --msg "simple:4: NEW :: cannotWithdrawZeroAmount"
+
+echo "******** Running:  simple:5 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotWithdrawFromInactiveReserve \
+           --msg "simple:5: NEW :: cannotWithdrawFromInactiveReserve"
+
+echo "******** Running:  simple:6 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotBorrowZeroAmount \
+           --msg "simple:6: NEW :: cannotBorrowZeroAmount"
+
+echo "******** Running:  simple:7 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotBorrowOnInactiveReserve \
+           --msg "simple:7: NEW :: cannotBorrowOnInactiveReserve"
+
+echo "******** Running:  simple:8 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotBorrowOnReserveDisabledForBorrowing \
+           --msg "simple:8: NEW :: cannotBorrowOnReserveDisabledForBorrowing"
+
+echo "******** Running:  simple:9 ***************"
+certoraRun $CMN certora/conf/NEW-pool-simple-properties.conf \
+           --rule cannotBorrowOnFrozenReserve \
+           --msg "simple:9: NEW :: cannotBorrowOnFrozenReserve"
+
 
